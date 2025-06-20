d9752851944f194dd2e9311ac204e109fc401fff
Perfect03 <zlobin_gg52926@mail.ru>
2025-06-20 16:03:28 +0500

    [BACK] refactor

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,10 +1,26 @@
-import {Module} from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectsModule } from './projects/projects.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService]
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: true,
+    }),
+    ProjectsModule,
+  ],
 })
 export class AppModule {}
--- Backend/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@nestjs/common"
-
-@Injectable()
-export class AppService {
-    getProjects() {
-        return [{id: 1, name: 'kdlolymp'}, {id: 2, name: 'tekme'}]
-    }
-}
